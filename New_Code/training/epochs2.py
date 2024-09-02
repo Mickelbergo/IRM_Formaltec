8691a1ed3b4346113a8012866818c466f19961b7
Mickelbergo <copetti00@gmail.com>
2024-09-02 14:35:42 +0200

    add custom loss function

--- New_Code/training/epochs2.py
+++ New_Code/training/epochs2.py
@@ -95,7 +95,7 @@ class TrainEpoch(Epoch):
         seg_loss = self.segmentation_loss_fn(y_pred, mask)
         #class_loss = self.classification_loss_fn(class_pred, mask_class)
 
-        loss = seg_loss + class_loss
+        loss = seg_loss + 0 #class_loss
         loss.backward()
 
         # Update the model's parameters
@@ -122,6 +122,6 @@ class ValidEpoch(Epoch):
             seg_loss = self.segmentation_loss_fn(y_pred, mask)
             #class_loss = self.classification_loss_fn(class_pred, mask_class)
 
-            loss = seg_loss + class_loss
+            loss = seg_loss + 0# class_loss
         
         return loss, y_pred, class_pred
